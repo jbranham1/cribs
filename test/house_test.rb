b66afa947b10e1934cc17483e291290def80057e
Jenny Branham <branhamjenny@gmail.com>
2020-12-09 14:26:36 -0600

    Finish house test

--- test/house_test.rb
+++ test/house_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/pride'
 require 'minitest/autorun'
 require './lib/house'
+require './lib/room'
 
 class HouseTest < MiniTest::Test
   def test_it_exists
@@ -12,7 +13,7 @@ class HouseTest < MiniTest::Test
   def test_it_has_readable_attributes
     house = House.new("$400000", "123 sugar lane")
 
-    assert_equal "$400000", house.price
+    assert_equal 400000, house.price
     assert_equal "123 sugar lane", house.address
     assert_equal [], house.rooms
   end
